4c7af1454a28ca83997e0b3a6d69a98e1d4972b0
skang9810 <suhjin9810@gmail.com>
2024-09-16 21:52:32 +0900

    feat: mainpage posts

---
+++ models/StudyContestPost.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose"); // 몽구스를 가져온다.
+const Schema = mongoose.Schema;
+
+const postSchema = mongoose.Schema(
+  {
+    writer: {
+      type: Schema.Types.ObjectId, // ObjectId 하나의 정보로 user에 있는 모든 정보들을 가져올 수 있다.
+      ref: "User", // User에 생성된 모든 정보를 가져온다.
+    },
+    title: {
+      type: String,
+      maxlength: 50,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    type: {
+      //스터디 or 대회
+      type: String,
+      required: true,
+    },
+    registeredNum: {
+      //모집 인원 수
+      type: Number,
+      required: true,
+    },
+    deadline: {
+      type: Date,
+      required: true,
+    },
+    meetingMethod: {
+      //온라인, 오프라인, 둘다, 미정
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const StudyContestPost = mongoose.model("StudyContestPost", postSchema); // 스키마를 모델로 감싸준다.
+
+module.exports = { StudyContestPost }; // 다른 곳에서도 사용할 수 있도록 export 해준다.
