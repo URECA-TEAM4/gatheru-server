81ecfe80b830a4ec90cde0b318d78225c358c1a5
JUJEOP <dapoo69@naver.com>
2024-09-26 15:46:30 +0900

    studyContests 수정

--- routes/studyContests.js
+++ routes/studyContests.js
@@ -12,14 +12,11 @@ router.get("/:postId", (req, res) => {
     .then((post) => res.json(post))
     .catch((err) => res.json(err));
 });
-router.post("/add", auth, (req, res) => {
+router.post("/add",(req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
-  const studyContestPost = new StudyContestPost({
-    ...req.body,
-    writer: req.user._id, 
-  });
+  const StudyContestPost = new StudyContestPost(...req.body);
 
-  studyContestPost.save((err, postInfo) => {
+  StudyContestPost.save((err, postInfo) => {
     // 몽고디비에서 오는 메소드
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
