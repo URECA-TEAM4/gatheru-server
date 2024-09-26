976645e85fcb17284d530eab7c2918a0ab815b6d
JUJEOP <dapoo69@naver.com>
2024-09-26 10:34:09 +0900

    mogako도 불러오게 수정

--- routes/myposts.js
+++ routes/myposts.js
@@ -5,9 +5,15 @@ const { auth } = require("../middleware/auth"); // 사용자 인증 미들웨어
 const { StudyContestPost } = require("../models/StudyContestPost") // 모델 스키마 가져오기
 // 사용자가 작성한 글 가져오기 (인증된 사용자만 접근 가능)
 router.get("/myposts", auth, (req, res) => {
-  StudyContestPost.find({ writer: req.user._id }) // writer 필드가 로그인된 사용자와 일치하는 게시글 찾기
-    .then(posts => res.json(posts))
-    .catch(err => res.json({ success: false, err }));
+  Promise.all([
+    StudyContestPost.find({ writer: req.user._id }), // StudyContestPost에서 글 가져오기
+    MogakoPost.find({ writer: req.user._id }) // MogakoPost에서 글 가져오기
+  ])
+  .then(([studyContestPosts, mogakoPosts]) => {
+    const allPosts = [...studyContestPosts, ...mogakoPosts]; // 두 게시글 목록을 하나로 합침
+    res.json(allPosts); // 합쳐진 게시글 목록을 응답
+  })
+  .catch(err => res.json({ success: false, err }));
 });
 
 // 특정 글 가져오기 (ID로 조회)
